7b16b058212b2debad1f25afbb4cb5c65b58d2d7
athenalauht <athenaht@connect.hku.hk>
2021-04-09 01:19:59 +0800

    updated

--- create_map.cpp
+++ create_map.cpp
@@ -8,8 +8,8 @@
 #include <time.h>
 
 using namespace std;
-void create_map()
-{
+int main() {
+
   //map size of 75*75
 
   const int SIZE_X = 100 - 1;
@@ -40,7 +40,7 @@ void create_map()
 
   while ((cur_x != SIZE_X - 2) || (cur_y != SIZE_Y - 2)){
 
-    temp_rand = rand() % 8;
+    temp_rand = rand() % 9;
     switch (temp_rand){
 
       case 0:
@@ -51,6 +51,7 @@ void create_map()
       case 1:
       case 4:
       case 7:
+      case 8:
       temp_cur_x = cur_x + 1;
       break;
 
@@ -79,47 +80,50 @@ void create_map()
 
     }
 
-    cur_x = 2, cur_y = 2;
-    while ((cur_x != SIZE_X - 2) || (cur_y != SIZE_Y - 2)){
 
-      temp_rand = rand() % 7;
-      switch (temp_rand){
+      cur_x = 2, cur_y = 2;
 
-        case 0:
-        case 6:
-        temp_cur_y = cur_y - 1;
-        break;
+      while ((cur_x != SIZE_X - 2) || (cur_y != SIZE_Y - 2)){
 
-        case 1:
-        case 4:
+        temp_rand = rand() % 9;
+        switch (temp_rand){
 
-        temp_cur_x = cur_x + 1;
-        break;
+          case 0:
+          case 6:
+          temp_cur_y = cur_y - 1;
+          break;
 
-        case 2:
-        case 5:
-        temp_cur_y = cur_y + 1;
-        break;
+          case 1:
+          case 4:
+          case 7:
+          case 8:
+          temp_cur_x = cur_x + 1;
+          break;
 
-        case 3:
-        temp_cur_x = cur_x - 1;
-        break;
+          case 2:
+          case 5:
+          temp_cur_y = cur_y + 1;
+          break;
+
+          case 3:
+          temp_cur_x = cur_x - 1;
+          break;
+
+          }
+
+          if ((temp_cur_x <= SIZE_X - 2) && (temp_cur_y <= SIZE_Y - 2) && (temp_cur_x >= 2) && (temp_cur_y >= 2)){
+
+            cur_x = temp_cur_x;
+            cur_y = temp_cur_y;
+
+            path << cur_x << " ";
+            path << cur_y << '\n';
+
+
+          }
+
 
         }
-
-        if ((temp_cur_x <= SIZE_X - 2) && (temp_cur_y <= SIZE_Y - 2) && (temp_cur_x >= 2) && (temp_cur_y >= 2)){
-
-          cur_x = temp_cur_x;
-          cur_y = temp_cur_y;
-
-          path << cur_x << " ";
-          path << cur_y << '\n';
-
-
-        }
-
-
-      }
 
 
   path.close();
