fa65e8d7a08843830243c0e6647ea4862ab311a0
athenalauht <athenaht@connect.hku.hk>
2021-04-12 16:06:35 +0800

    finish save.cpp for saving the position of the chess when terminate

--- save.cpp
+++ save.cpp
@@ -12,4 +12,29 @@
 
 using namespace std;
 
-void store_profile();
+void store_profile(player[3]){
+
+  //open p1.txt
+  ofstream p1;
+  p1.open("p1.txt");
+
+  p1 << player[1].emperor_y << " " << player[1].emperor_x << '\n';
+  p1 << player[1].knight_y << " " << player[1].knight_x << '\n';
+  p1 << player[1].soldier_y << " " << player[1].soldier_x << '\n';
+  p1 << player[1].assassin_y << " " << player[1].assassin_x << '\n';
+
+  p1.close();
+
+  //open p2.txt
+  ofstream p2;
+  p2.open("p2.txt");
+
+  p2 << player[2].emperor_y << " " << player[2].emperor_x << '\n';
+  p2 << player[2].knight_y << " " << player[2].knight_x << '\n';
+  p2 << player[2].soldier_y << " " << player[2].soldier_x << '\n';
+  p2 << player[2].assassin_y << " " << player[2].assassin_x << '\n';
+
+  p2.close();
+
+
+}
