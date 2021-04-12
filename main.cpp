2e80c2e8b557f7d8f449d1316dcabb4fea34e2d1
athenalauht <athenaht@connect.hku.hk>
2021-04-12 15:37:22 +0800

    updated

--- main.cpp
+++ main.cpp
@@ -67,63 +67,62 @@ int main()
   create_path();
   initial_path(map);
   create_teleport(teleport_1, teleport_2, map);
-  print_map(map);
 
   //--------------------------------------------
 
-  // cout << "This is the initial map setting. Player 1 will first start his/her round" << endl;
-  //
-  // player[1].emperor_x = 0;
-  // player[1].emperor_y = 0;
-  // player[1].knight_x = 0;
-  // player[1].knight_y = 0;
-  // player[1].soldier_x = 0;
-  // player[1].soldier_y = 0;
-  // player[1].assassin_x = 0;
-  // player[1].assassin_y = 0;
-  //
-  // player[2].emperor_x = 0;
-  // player[2].emperor_y = 0;
-  // player[2].knight_x = 0;
-  // player[2].knight_y = 0;
-  // player[2].soldier_x = 0;
-  // player[2].soldier_y = 0;
-  // player[2].assassin_x = 0;
-  // player[2].assassin_y = 0;
-  //
-  // // to locate the starting point of each player
-  // int player1_starting_point[2];
-  // int player2_starting_point[2];
-  //
-  // for (int i = 0; i < 23; ++i) {
-  //   if (map[i][2] == 1 || map[i][2] == 9) {
-  //     player1_starting_point[0] = 2;
-  //     player1_starting_point[1] = i;
-  //     break;
-  //   }
-  // }
-  //
-  // for (int i = 22; i > 1; --i) {
-  //   if (map[i][97] == 1 || map[i][2] == 9) {
-  //     player2_starting_point[0] = 97;
-  //     player2_starting_point[1] = i;
-  //     break;
-  //   }
-  // }
-  // // to start the game
-  // int count = 1;
-  // bool termination = false;
-  // while (!termination) {
-  //
-  //   if (count % 2 == 0) {
-  //     // termination = player2_interface(player2_starting_point[2]);
-  //     count += 1;
-  //   }
-  //
-  //   else {
-  //     termination = player1_interface( player1_starting_point, map, teleport_1, teleport_2 );
-  //     count += 1;
-  //   }
-  // }
+  cout << "This is the initial map setting. Player 1 will first start his/her round" << endl;
+
+  player[1].emperor_x = 0;
+  player[1].emperor_y = 0;
+  player[1].knight_x = 0;
+  player[1].knight_y = 0;
+  player[1].soldier_x = 0;
+  player[1].soldier_y = 0;
+  player[1].assassin_x = 0;
+  player[1].assassin_y = 0;
+
+  player[2].emperor_x = 0;
+  player[2].emperor_y = 0;
+  player[2].knight_x = 0;
+  player[2].knight_y = 0;
+  player[2].soldier_x = 0;
+  player[2].soldier_y = 0;
+  player[2].assassin_x = 0;
+  player[2].assassin_y = 0;
+
+  // to locate the starting point of each player
+  int player1_starting_point[2];
+  int player2_starting_point[2];
+
+  for (int i = 0; i < 23; ++i) {
+    if (map[i][2] == 1 || map[i][2] == 9) {
+      player1_starting_point[0] = 2;
+      player1_starting_point[1] = i;
+      break;
+    }
+  }
+
+  for (int i = 22; i > 1; --i) {
+    if (map[i][97] == 1 || map[i][2] == 9) {
+      player2_starting_point[0] = 97;
+      player2_starting_point[1] = i;
+      break;
+    }
+  }
+  // to start the game
+  int count = 1;
+  bool termination = false;
+  while (!termination) {
+
+    if (count % 2 == 0) {
+      // termination = player2_interface(player2_starting_point[2]);
+      count += 1;
+    }
+
+    else {
+      termination = player1_interface( player1_starting_point, map, teleport_1, teleport_2 );
+      count += 1;
+    }
+  }
 
 }
